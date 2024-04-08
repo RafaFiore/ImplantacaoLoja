a457b7735ee9f0f57e53feae8c6df483ffaa3c49
Rafael Fiorentini <rafael.fiorentini@endurance.com>
2024-04-08 12:57:29 -0300

    alterado funções e payloads para trabalhar no ambiente de produção

--- Implantacao.py
+++ Implantacao.py
@@ -29,7 +29,7 @@ class Implantacao:
     # Procurar no Zendesk os tickets em aberto da RD
     def search_rd_tickets(self):
         query = 'type%3Aticket%20requester%3Arafael.fiorentini%40newfold.com%20status%3Aopen%20status%3Anew%20Implanta%C3%A7%C3%A3o%20Ecommerce'
-        url = self.zenAPI.new_urls[self.brand] + self.zenAPI.search + query
+        url = self.zenAPI.prod_url + self.zenAPI.search + query
         response = self.zenAPI.get_request(url, new_instance=True)
         result = response['results']
         return result
@@ -41,7 +41,7 @@ class Implantacao:
 
     # Verificar se o cliente já tem cadastro no Zendesk
     def check_zd_user(self, cst_email):
-        url = self.zenAPI.new_urls[self.brand] + self.zenAPI.search_users + cst_email
+        url = self.zenAPI.prod_url + self.zenAPI.search_users + cst_email
         response = self.zenAPI.get_request(url, new_instance=True)
         user = response['users']
         return user
@@ -70,9 +70,11 @@ class Implantacao:
     # Criar ticket na Zendesk
     def create_ticket(self, cst_email):
         subject, comment = self.zd_template()
-        submitter_id = 24966456979859 # TROCAR PARA PROD
-        group_id = 24966089236755 # TROCAR PARA PROD
-        brand_id = 26558151214099 # TROCAR PARA PROD
+        # submitter_id = 24966456979859 # TROCAR PARA PROD
+        # group_id = 24966089236755 # TROCAR PARA PROD
+        # brand_id = 26558151214099 # TROCAR PARA PROD
+        group_id = 27881687140627
+        submitter_id = 26772636303507
         payload = {
             "ticket": {
                 "subject": f"{subject}",
@@ -86,7 +88,7 @@ class Implantacao:
                 "group_id": group_id,
                 "is_public": "true",
                 "status": "pending",
-                "brand_id": brand_id,
+                "brand_id": self.zenAPI.brands['loja'],
                 "tags": ["dl_implantacao_abertura"],
                 "fields": [
                     {
@@ -108,10 +110,11 @@ class Implantacao:
         return response
 
     # Atualizar tickets na Zendesk
-    def update_tickets(self, ticket_id, comment, status, situacao_ticket):
-        url = self.zenAPI.new_urls[self.brand] + self.zenAPI.ticekts + ticket_id
+    def update_tickets(self, ticket_id, comment, status, situacao_ticket, group_id):
+        url = self.zenAPI.prod_url + self.zenAPI.ticekts + ticket_id
         print(url)
-        submitter_id = 24966456979859  # TROCAR PARA PROD
+        # submitter_id = 24966456979859  # TROCAR PARA PROD
+        submitter_id = 26772636303507
         payload = {
             "ticket": {
                 "comment": {
@@ -120,6 +123,7 @@ class Implantacao:
                 },
                 "submitter_id": submitter_id,
                 "status": f"{status}",
+                "group_id": group_id,
                 "fields": [
                     {
                         "id": 26804463897363,
@@ -172,6 +176,7 @@ class Implantacao:
                 rd_ticket_id = str(i['id'])
                 rd_ticket_date = i['created_at']
                 subject = i['subject']
+                rd_group_id = i['group_id']
                 cst_email = subject.split('-')[1].strip()
                 cst_name = subject.split('-')[2].strip()
                 rd_lead_url = re.findall(rd_lead_pattern, i['description'])[0]
@@ -194,24 +199,34 @@ class Implantacao:
                 job_status_response = self.zenAPI.get_request(job_status_url, new_instance=True)
                 job_status = job_status_response['job_status']['status']
                 print(ticket_id)
-                rd_comment = f"""Ticket aberto com o cliente: {ticket_id}
+                cst_tkt_link = 'https://atendimentohgbrasil.zendesk.com/agent/tickets/' + ticket_id
+                rd_comment = f"""Ticket aberto com o cliente: {cst_tkt_link}
                 """
-                rd_ticket_response = self.update_tickets(rd_ticket_id, rd_comment, status='solved',
-                                                         situacao_ticket='dl_atendido')
+                rd_ticket_response = self.update_tickets(rd_ticket_id,
+                                                         rd_comment,
+                                                         status='pending',
+                                                         situacao_ticket='dl_atendido',
+                                                         group_id=rd_group_id)
                 print(rd_ticket_response)
-                cst_comment = f"""Ticket da RD: {rd_ticket_id}
+                rd_tkt_link = 'https://atendimentohgbrasil.zendesk.com/agent/tickets/' + rd_ticket_id
+                cst_comment = f"""Ticket da RD: {rd_tkt_link}
                 Link para dados do cliente: {rd_lead_url}
                 """
                 if job_status == 'completed':
-                    cst_ticket_response = self.update_tickets(ticket_id, cst_comment, status='pending',
+                    cst_ticket_response = self.update_tickets(ticket_id,
+                                                              cst_comment,
+                                                              status='pending',
                                                               situacao_ticket='dl_aguardando_cliente')
                     ticket_date = datetime.now().replace(microsecond=0)
                     print(cst_ticket_response)
                 else:
                     print('Aguardando criação do ticket')
                     time.sleep(15)
-                    cst_ticket_response = self.update_tickets(ticket_id, cst_comment, status='pending',
-                                                              situacao_ticket='dl_aguardando_cliente')
+                    cst_ticket_response = self.update_tickets(ticket_id,
+                                                              cst_comment,
+                                                              status='pending',
+                                                              situacao_ticket='dl_aguardando_cliente',
+                                                              group_id=27881687140627)
                     ticket_date = datetime.now().replace(microsecond=0)
                     print(cst_ticket_response)
                 self.add_db_values(ticket_id, rd_ticket_id, cst_email, cst_name, rd_ticket_date, ticket_date, 'pending')
@@ -235,7 +250,11 @@ class Implantacao:
                 Prosseguir com implatanção.
                 Link para respostas do forms do cliente: {form_link} 
                 """
-                response = self.update_tickets(ticket_id, comment, status='open', situacao_ticket='dl_aguardando_consultor')
+                response = self.update_tickets(ticket_id,
+                                               comment,
+                                               status='open',
+                                               situacao_ticket='dl_aguardando_consultor',
+                                               group_id=26367840632851)
                 self.db_conn.update_ticket(ticket_id)
             else:
                 print('Ticket não encontrado com o email do cliente')
@@ -244,5 +263,5 @@ class Implantacao:
 
 if __name__ == "__main__":
     implant = Implantacao('dloja')
-    implant.process_implantacao()
-    # implant.process_form_answers()
+    # implant.process_implantacao()
+    implant.process_form_answers()
